f6564c02d34a5ec3e94f1c80635896dd4a2d668b
vagrant <vagrant@vagrant.vm>
2020-10-06 04:28:44 +0000

    finished homework

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,28 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
 
 
-def print_melon(name, seedless, price):
+def print_melon(melons):
     """Print each melon with corresponding attribute information."""
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+        print('===============================')
+        
+print(print_melon(melons))
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
+
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
